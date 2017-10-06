ff69998f88eb51e26ba88e44415c8b5aca273dec
Max Tweddell <maxt.nz@gmail.com>
2017-10-07 11:02:22 +1300

    first commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+var server = require('./server')
+const request = require('superagent')
+
+var PORT = process.env.PORT || 3000
+
+server.listen(PORT, function () {
+  console.log('Listening on port', PORT)
+})
+
